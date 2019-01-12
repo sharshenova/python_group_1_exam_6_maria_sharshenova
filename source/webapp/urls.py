0096e57577d23f864304af8c33b660ce67312b3d
Maria Sharshenova <maria@sharshenov.com>
2019-01-12 15:20:58 +0600

    Add PostCreateView

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from webapp.views import welcome_view, PostListView, PostDetailView
+from webapp.views import welcome_view, PostListView, PostDetailView, PostCreateView
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -11,6 +11,7 @@ urlpatterns = [
     path('', welcome_view, name='welcome'),
     path('posts', PostListView.as_view(), name='post_list'),
     path('posts/<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('posts/create', PostCreateView.as_view(), name='post_create'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from webapp.models import Post
-from django.views.generic import ListView, DetailView
-#CreateView, UpdateView, DeleteView, FormView
+from django.views.generic import ListView, DetailView, CreateView
+#UpdateView, DeleteView, FormView
 from webapp.forms import PostForm
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import redirect, get_object_or_404
@@ -25,3 +25,20 @@ class PostListView(LoginRequiredMixin, ListView):
 class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    form_class = PostForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('webapp:post_detail', kwargs={'pk': self.object.pk})
+
+
+
