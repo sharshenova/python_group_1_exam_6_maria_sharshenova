74563844f0a2ceb26d4bc949bb1764864ba569f3
Maria Sharshenova <maria@sharshenov.com>
2019-01-12 12:39:16 +0600

    Add Models

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='userInfo', verbose_name='Пользователь')
+    phone = models.CharField(max_length=50, verbose_name='Телефон')
+    photo = models.ImageField(verbose_name='Фотография', null=True, blank=True)
+    friends = models.ManyToManyField(User, blank=True, related_name="friends", verbose_name="Друзья")
+
+    def __str__(self):
+        return self.user
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    text = models.TextField(max_length=2000, verbose_name='Текст')
+    date = models.DateTimeField(default=datetime.now, verbose_name='Дата')
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='posts_by_user', verbose_name='Автор поста')
+
+    def __str__(self):
+        return self.title
+
